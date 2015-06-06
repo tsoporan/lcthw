bf2e96535d8dab2ce229c80293ac109e475c5475
Titus Soporan <titus@tsoporan.com>
2015-06-06 13:48:02 -0400

    ex14 extra credit

--- ex14.c
+++ ex14.c
@@ -1,35 +1,30 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
-int can_print_it(char ch);
-void print_letters(char arg[]);
+void print_letters(char arg[], int size);
 void print_arguments(int argc, char* argv[]);
 
 void print_arguments(int argc, char* argv[]) {
     int i = 0;
     for (; i < argc; i++) {
-        print_letters(argv[i]);
+        print_letters(argv[i], strlen(argv[i]));
     }
 }
 
-void print_letters(char arg[]) {
+void print_letters(char arg[], int size) {
     int i = 0;
 
-    for(; arg[i] != '\0'; i++) {
+    for(; i < size; i++) {
         char ch = arg[i];
 
-        if (can_print_it(ch)) {
+        if (isalpha(ch) || isblank(ch)) {
             printf("'%c' == %d ", ch, ch);
         }
     }
 
     printf("\n");
 }
-
-int can_print_it(char ch) {
-    return isalpha(ch) || isblank(ch);
-}
-
 
 int main(int argc, char* argv[]) {
 
